69c89607742e7d31ef3e33cf7aa98ff59f90f1a3
dmitry <rbdeman@yandex.ru>
2022-11-22 13:40:10 +0300

    коммит для четвертой задачи

---
+++ zadacha_four/Program.cs
@@ -0,0 +1,16 @@
+﻿мConsole.Clear();
+Console.Write("введи число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+while (count < number+1)
+{
+    if (count%2 == 0)
+    {
+        Console.WriteLine(count);
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+}
