f2d14510606d2abf56900047050d70134401f866
dmitry <rbdeman@yandex.ru>
2022-11-22 13:21:58 +0300

    сделал первую задачу

---
+++ zadacha_one/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("введи первое число: ");
+int number_one = int.Parse(Console.ReadLine());
+Console.Write("введи второе число: ");
+int number_two = int.Parse(Console.ReadLine());
+if (number_one>number_two)
+{
+    Console.WriteLine($"число {number_one} больше числа {number_two}");
+}
+if (number_one<number_two)
+{
+    Console.WriteLine($"число {number_one} меньше числа {number_two}");
+}
+if (number_one == number_two)
+{
+    Console.WriteLine("числа одинаковы");
+}
