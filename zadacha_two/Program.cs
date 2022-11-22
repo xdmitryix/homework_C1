a100e8cd297c13bb90f293d9db32b3739ad0c664
dmitry <rbdeman@yandex.ru>
2022-11-22 13:30:49 +0300

    коммит ко второй задаче

---
+++ zadacha_two/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.Write("введи первое число: ");
+int number_one = int.Parse(Console.ReadLine());
+Console.Write("введи второе число: ");
+int number_two = int.Parse(Console.ReadLine());
+Console.Write("введи третье число: ");
+int number_three = int.Parse(Console.ReadLine());
+int max = 0;
+if (number_one>number_two)
+{
+    max = number_one;
+}
+else
+{
+    max = number_two;
+}
+if (number_three>max)
+{
+    max = number_three;
+}
+Console.WriteLine($"максимальное число: {max}");
